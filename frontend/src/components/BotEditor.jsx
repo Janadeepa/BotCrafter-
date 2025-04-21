02788c3882c2f1b8e1487f7eed7ee987418ee3e6
Yuthmika Janadeepa Priyankara <136349851+Janadeepa@users.noreply.github.com>
2025-04-21 04:00:13 -0700

    Delete frontend/src directory

--- frontend/src/components/BotEditor.jsx
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-const BotEditor = () => <div className="p-4 border rounded">[Bot Flow Editor Placeholder]</div>;
-export default BotEditor;
--- frontend/src/components/PlatformSelector.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const PlatformSelector = ({ selected, onChange }) => (
-  <select value={selected} onChange={e => onChange(e.target.value)}>
-    <option value="discord">Discord</option>
-    <option value="telegram">Telegram</option>
-    <option value="slack">Slack</option>
-  </select>
-);
-export default PlatformSelector;
